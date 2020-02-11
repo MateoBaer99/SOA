035930915834a5015c0b5f9d4ed74160b0960a79
paki6340 <ahmed.kriket@orange.com>
2020-02-11 11:58:36 +0100

    Ajout des logs : requêtes et réponses

--- src/main/java/org/universite/VentesApplication.java
+++ src/main/java/org/universite/VentesApplication.java
@@ -1,6 +1,9 @@
 package org.universite;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.boot.SpringApplication;
@@ -9,20 +12,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @ApplicationPath("/api")
-public class VentesApplication extends ResourceConfig{
-    
+public class VentesApplication extends ResourceConfig {
+  
+   private static final Logger LOGGING_INTERCEPTOR = Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME);
+   // Taille maximale du message à logger (tronqué si requête ou réponse plus volumineuse)
+   private static final int MAX_ENTITY_SIZE=8192;
+   
     public VentesApplication () {
-        // Register resources and providers using package-scanning.
+        // Scanne l'ensemble des classes du package à la recherche de ressources Jax-rs (@Path)
         packages("org.universite.ventes.api");
 
         // Register my custom provider - not needed if it's in my.package.
        // register(SecurityRequestFilter.class);
-        // Register an instance of LoggingFilter.
-    //    register(new LoggingFilter(LOGGER, true));
+       
+        // Activation des logs (traces des requêtes et réponses
+        register(new LoggingFeature(LOGGING_INTERCEPTOR, Level.INFO,LoggingFeature.Verbosity.PAYLOAD_TEXT,MAX_ENTITY_SIZE));
 
+        
         // Permet d'activer les traces : voir en-têtes HTTP de la réponse
         property(ServerProperties.TRACING, "ALL");
-        property(ServerProperties.PROCESSING_RESPONSE_ERRORS_ENABLED,"true");
+
     }
 
 	public static void main(String[] args) {
